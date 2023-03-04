8226ca92273515297735f305e40410ccc26c82b7
godhands <3124406837@qq.com>
2023-03-04 19:00:45 +0800

    finish the facelogin

--- Eyes/static/js/src/face/zbase.js
+++ Eyes/static/js/src/face/zbase.js
@@ -4,47 +4,86 @@ class GodEyesFace {
         this.$face = $(
             `
                 <div>
+    <video id="video" width="500" height="600" controls>
+    </video>
+    <canvas id="canvas" width="500" height="600"></canvas>
+    <div>
+        <button id="capture">拍照</button>
+        <button id="close">关闭摄像头</button>
+    </div>
 
-    <body>
-    <video id="video" width="640" height="480" autoplay></video>
-    <button id="snap">Snap Photo</button>
-    <canvas id="canvas" width="640" height="480"></canvas>
-    <h2>Please click the button</h2>
-    </body>
-    <script type="text/javascript">
-        var aVideo = document.getElementById('video');
-        var aCanvas = document.getElementById('canvas');
-        var ctx = aCanvas.getContext('2d');
+    <script>
+        let video = document.getElementById('video');
+        let canvas = document.getElementById('canvas');
+        let context = canvas.getContext('2d');
+        var mediaStreamTrack;
 
-        navigator.getUserMedia = navigator.getUserMedia ||
-            navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia ||
-            navigator.msGetUserMedia; //获取媒体对象（这里指摄像头）
-        navigator.getUserMedia({
-            video: true
-        }, gotStream, noStream); //参数1获取用户打开权限；参数二是一个回调函数，自动传入视屏流，成功后调用，并传一个视频流对象，参数三打开失败后调用，传错误信息
-
-        function gotStream(stream) {
-
-            // video.src = URL.createObjectURL(stream); // 老写法
-            aVideo.srcObject = stream;
-
-            aVideo.onerror = function () {
-                stream.stop();
-            };
-            stream.onended = noStream;
-            aVideo.onloadedmetadata = function () {
-                // alert('Success!!!');
-            };
+        //访问用户媒体设备的兼容方法
+        function getUserMedia(constraints, success, error) {
+            if (navigator.mediaDevices.getUserMedia) {
+                //最新的标准API
+                navigator.mediaDevices.getUserMedia(constraints).then(success).catch(error);
+            } else if (navigator.webkitGetUserMedia) {
+                //webkit核心浏览器
+                navigator.webkitGetUserMedia(constraints, success, error)
+            } else if (navigator.mozGetUserMedia) {
+                //firfox浏览器
+                navigator.mozGetUserMedia(constraints, success, error);
+            } else if (navigator.getUserMedia) {
+                //旧版API
+                navigator.getUserMedia(constraints, success, error);
+            }
         }
 
-        function noStream(err) {
-            alert(err);
+        function success(stream) {
+            //兼容webkit核心浏览器
+            let CompatibleURL = window.URL || window.webkitURL;
+            //将视频流设置为video元素的源
+            //video.src = CompatibleURL.createObjectURL(stream);
+
+            // 播放
+            video.srcObject = stream;
+            video.play();
+            mediaStreamTrack = stream
         }
 
-        document.getElementById("snap").addEventListener("click", function () {
-            ctx.drawImage(aVideo, 0, 0, 640, 480); //将获取视频绘制在画布上
-            alert("login in");
+        function error(error) {
+            alert('调用摄像头失败')
+        }
+
+        if (navigator.mediaDevices.getUserMedia || navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia) {
+            getUserMedia({ video: { width: 300, height: 400 } }, success, error);
+        } else {
+            alert('不支持访问用户媒体');
+        }
+
+        // 拍照
+        document.getElementById('capture').addEventListener('click', function () {
+            context.drawImage(video, 0, 0, 500, 600);
+            // photoSrc 就是拍照得到的base64的图片数据，将数据传递给后端就可以了
+            var photoSrc = document.getElementById("canvas").toDataURL("image/jpeg", 0.8);
+            $.ajax({
+                url: "http://localhost:8000/settings/getinfo/",
+                type: "GET",
+                data: {
+                    IMG: photoSrc,
+                },
+                success: function (resp) {
+                   console.log(resp);
+                   if (resp.result === "success") {
+                       alert("Login Success!");
+                   } else {
+                       alert("Wrong!");
+                   }
+                }
+            });
+        })
+
+        // 关闭摄像头
+        document.getElementById('close').addEventListener('click', function () {
+            if (mediaStreamTrack) {
+                mediaStreamTrack.getTracks()[0].stop();
+            }
         });
     </script>
 </div>
--- Eyes/views/settings/getinfo.py
+++ Eyes/views/settings/getinfo.py
@@ -1,14 +1,88 @@
+import base64
+import uuid
+import re
+import cv2
+import glob
+import os
 from django.http import JsonResponse
 
 
-def getinfo_web(request):
+def getinfo_right(request):
     return JsonResponse({
         'result': "success",
-        'username': "hello, world",
     })
 
 
-def getinfo(request):
-    platform = request.GET.get('platform')
-    return getinfo_web(request)
+def getinfo_wrong(request):
+    return JsonResponse({
+        'result': "wrong",
+    })
 
+
+def decode_image(src):
+    # 1、信息提取
+    result = re.search("data:image/(?P<ext>.*?);base64,(?P<data>.*)", src, re.DOTALL)
+    if result:
+        ext = result.groupdict().get("ext")
+        data = result.groupdict().get("data")
+
+    else:
+        raise Exception("Do not parse!")
+
+    # 2、base64解码
+    img = base64.urlsafe_b64decode(data)
+
+    # 3、二进制文件保存
+    filename = "{}.{}".format(uuid.uuid4(), ext)
+    PATH = "./IMAGE/" + filename
+    with open(PATH, "wb") as f:
+        f.write(img)
+
+    return filename
+
+
+def trainer():
+    pass
+
+
+def check():
+    recogizer = cv2.face.LBPHFaceRecognizer_create()
+    recogizer.read('trainer/trainer.yml')
+    names = []
+    warningtime = 0
+
+    def face_detect_demo(img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 转换为灰度
+        face_detector = cv2.CascadeClassifier('./haarcascade_frontalface_alt2.xml')
+        face = face_detector.detectMultiScale(
+            gray, 1.1, 5, cv2.CASCADE_SCALE_IMAGE, (100, 100), (300, 300))
+        # face=face_detector.detectMultiScale(gray)
+        for x, y, w, h in face:
+            cv2.rectangle(img, (x, y), (x + w, y + h), color=(0, 0, 255), thickness=2)
+            cv2.circle(img, center=(x + w // 2, y + h // 2), radius=w //
+                                                                    2, color=(0, 255, 0), thickness=1)
+            # 人脸识别
+            ids, confidence = recogizer.predict(gray[y:y + h, x:x + w])
+            # print('标签id:',ids,'置信评分：', confidence)
+            if confidence > 80:
+                return False
+            else:
+                return True
+
+    WSI_MASK_PATH = './IMAGE/'  # 存放图片的文件夹路径
+    paths = glob.glob(os.path.join(WSI_MASK_PATH, '*.jpeg'))
+    paths.sort()
+    for i in paths:
+        print(i)
+        img = cv2.imread(i)
+        if face_detect_demo(img):
+            print("YES")
+        else:
+            print("NO")
+
+
+def getinfo(request):
+    platform = request.GET.get('IMG')
+    filename = decode_image(platform)
+    check()
+    return getinfo_right(request)
